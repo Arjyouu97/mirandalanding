4a9ca67fd02f62fee334681aab8a83dcea45cb29
Arjun Rajendran <ar5unrajendran@gmail.com>
2024-07-13 15:08:17 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Miranda Business Solutions</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Miranda Business Solutions" />
+       
       </main>
 
       <Footer />
